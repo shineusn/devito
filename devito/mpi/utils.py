53b32188622937e4100b897e93aeb2606e18ea63
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-07-13 13:42:44 +0200

    wip wip

--- devito/mpi/utils.py
+++ devito/mpi/utils.py
@@ -3,7 +3,7 @@ from itertools import product
 
 from devito.types import OWNED, HALO, LEFT, RIGHT
 
-__all__ = ['get_views']
+__all__ = ['get_views', 'derive_halo_updates']
 
 
 def get_views(f, fixed):
@@ -30,3 +30,31 @@ def get_views(f, fixed):
                 offsets.append(0)
         mapper[(dimension, side, region)] = (sizes, offsets)
     return mapper
+
+
+def derive_halo_updates(dspace):
+    """
+    Given a :class:`DataSpace`, return two mappers: ::
+
+        * ``dimension -> [(function, side, amount), ...]}``
+        * ``function -> [(dimension, side, amount), ...]
+
+    describing what halo exchanges are required.
+    """
+    dmapper = {}
+    fmapper = {}
+    for k, v in dspace.parts.items():
+        if k.grid is None:
+            continue
+        for i in v:
+            if i.dim not in k.grid.distributor.dimensions:
+                continue
+            lsize = k._offset_domain[i.dim].left - i.lower
+            if lsize > 0:
+                dmapper.setdefault(i.dim, []).append((k, LEFT, lsize))
+                fmapper.setdefault(k, []).append((i.dim, LEFT, lsize))
+            rsize = i.upper - k._offset_domain[i.dim].right
+            if rsize > 0:
+                dmapper.setdefault(i.dim, []).append((k, RIGHT, rsize))
+                fmapper.setdefault(k, []).append((i.dim, RIGHT, rsize))
+    return dmapper, fmapper
